c3bb0bf42c097a2cd65c1da1ba85c251d495fc6d
Florian Dorau <fdorau@it-economics.de>
2023-11-04 15:01:40 +0100

    Manage Backstage kubenertes integration

---
+++ infrastructure/cluster_info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONTROL_PANE_URL=$(kubectl cluster-info | grep 'Kubernetes control plane' |  grep -o 'https*://[^"]*')
+SA_TOKEN=$(kubectl -n default get secret backstage-sa-secret -o=json | jq -r '.data["token"]' | base64 --decode)
+
+echo "# ------------------------------------------ #"
+echo "# Please export the following env variables: #"
+echo "# ------------------------------------------ #"
+echo "export BACKSTAGE_SERVICE_ACCOUNT_TOKEN=$SA_TOKEN"
+echo ""
+echo "export BACKSTAGE_K8S_CONTROL_PANE_URL=$CONTROL_PANE_URL"
+echo ""
